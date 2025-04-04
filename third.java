4930e6f5cf9a8e7ae3f23c012352d3fb947b9e50
Mahek312 <mahekrehamanavar@gmail.com>
2025-04-04 09:16:37 +0530

    third

---
+++ third.java
@@ -0,0 +1,23 @@
+package java1;
+
+public class third {
+    
+
+        String model;
+        int year;
+     third(String m, int y) {
+            model = m;
+            year = y;
+        }
+      void displayCarDetails() {
+            System.out.println("Car Model: " + model);
+            System.out.println("Car Year: " + year);
+        }
+         public static void main(String[] args) {
+            third myCar = new third("BMW", 2020);
+            myCar.displayCarDetails();
+        
+    }
+    
+}
+
