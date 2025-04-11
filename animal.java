a15e704b84fbb6765718bff3a447cde855a8b509
Mahek312 <mahekrehamanavar@gmail.com>
2025-04-11 09:44:33 +0530

    save

---
+++ animal.java
@@ -0,0 +1,18 @@
+package java1;
+
+class animalsuper{
+    String color = "white"; 
+}
+class dog extends animalsuper{
+    String color = "Black";
+    void print(){
+        System.out.println(color);
+        System.out.println(super.color);
+    }
+}
+public class animal{
+public static void main(String[] args){
+    dog d = new dog();
+    d.print();
+}
+}
